a9bfb1dd598b8430f81f9d860b22493de1de9e27
Ryan Gonzalez <ryan.gonzalez064@gmail.com>
2021-02-12 22:47:47 -0600

    added problem 8 solution

---
+++ Problem8_StringToInt/Driver.java
@@ -0,0 +1,20 @@
+package Problem8_StringToInt;
+
+public class Driver {
+    public static void main(String[] args){
+        String negativeNumber = "-45";
+        String positiveNumber = "100";
+        String whiteSpaceNumber = "     -46";
+        int negativeNumberConverted = 0;
+        int positiveNumberConverted = 0;
+        int whiteSpaceNumberConverted = 0;
+
+        Solution algorithm = new Solution();
+        negativeNumberConverted = algorithm.myAtoi(negativeNumber);
+        positiveNumberConverted = algorithm.myAtoi(positiveNumber);
+        whiteSpaceNumberConverted = algorithm.myAtoi(whiteSpaceNumber);
+
+        System.out.println("Integer pos#: " + positiveNumberConverted + " Integer neg#: " + negativeNumberConverted);
+        System.out.println("Integer whitespace #: " + whiteSpaceNumberConverted);
+    }
+}
